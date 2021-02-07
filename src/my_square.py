c30c258469f6ae86101f6a96521008fe33a9d48e
Johne <johne.vang1@gmail.com>
2021-02-07 10:44:15 -0600

    added python script

---
+++ src/my_square.py
@@ -0,0 +1,13 @@
+def mySquare(y):
+	"""
+	takes a value and returns the squared value.
+
+	uses the * operator.
+	"""
+	return (y ** 2)
+
+def my_square2(x):
+	return (x * x)
+
+print(my_sqaure(42))
+print(my_square2(42))
